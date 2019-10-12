fc4211b44ce944cc5f2b55d694b12f0a579ff5fe
Kirmartuk <kirillmartuk@gmail.com>
2019-10-12 12:51:32 +0700

    fix bug with request

--- src/main/java/com/martyuk/shortLink/ShortLinkApplication.java
+++ src/main/java/com/martyuk/shortLink/ShortLinkApplication.java
@@ -2,10 +2,16 @@ package com.martyuk.shortLink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.martyuk.shortLink.data")
+@EntityScan(basePackages = "com.martyuk.shortlink.entity")
+@EnableTransactionManagement
+@EnableScheduling
 public class ShortLinkApplication {
 
 	public static void main(String[] args) {
